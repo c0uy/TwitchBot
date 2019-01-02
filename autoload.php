b6c761c8d387b91d912a2b55951c3b6c4ede7886
couy <toony.f@gmail.com>
2019-01-02 14:48:47 +0100

    Define 3 colors & globally instanciation of Log class

--- autoload.php
+++ autoload.php
@@ -1,6 +1,11 @@
 <?php
+define('COLOR_DEFAULT', 'NORMAL');
+define('COLOR_ERROR', 'RED');
+define('COLOR_SUCCESS', 'GREEN');
 
 // PHP Classes
 require_once 'class/Log.php';
 require_once 'class/Socket.php';
 require_once 'class/TwitchIRC.php';
+
+$log = new Log();
