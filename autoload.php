67fd3e8b48e7da25e200b5f50bd1f3e98f26d8af
AnthonydCF <35349899+AnthonydCF@users.noreply.github.com>
2019-01-03 01:49:36 +0100

    Define bot colors

--- autoload.php
+++ autoload.php
@@ -5,6 +5,8 @@ define('COLOR_DEFAULT', 'NORMAL');
 define('COLOR_INFO', 'YELLOW');
 define('COLOR_ERROR', 'RED');
 define('COLOR_SUCCESS', 'GREEN');
+define('COLOR_BOT_PING', 'BROWN');
+define('COLOR_BOT_MESSAGE', 'LIGHT_CYAN');
 
 // PHP Classes
 require_once 'class/Log.php';
